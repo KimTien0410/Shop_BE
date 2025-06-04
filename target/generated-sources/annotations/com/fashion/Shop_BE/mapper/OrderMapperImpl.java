bd6354e1b390b0267c5e32a8c5a9f5639b221333
tientran <kimtientran0410@gmail.com>
2025-06-04 17:41:18 +0700

    hot fix payos

--- target/generated-sources/annotations/com/fashion/Shop_BE/mapper/OrderMapperImpl.java
+++ target/generated-sources/annotations/com/fashion/Shop_BE/mapper/OrderMapperImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-05-29T20:44:09+0700",
+    date = "2025-06-04T17:14:17+0700",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.1 (Oracle Corporation)"
 )
 @Component
